de75599ee5aa9d3c79e5d37857c5f73b95cab411
pipinstallsympy <misha.zherbin.05@gmail.com>
2025-07-28 20:32:20 +0300

    Ух ты, это приложение???????

--- SQLlogic/SQLLogic.cs
+++ SQLlogic/SQLLogic.cs
@@ -16,6 +16,35 @@ public static class SQLLogic
             return message;
         }
     }
+    
+    public static string[][] SQLReadAllMatrix(MySqlConnection connection, string table)
+    {
+        string cmdCount = $"SELECT COUNT(*) FROM {table}";
+        int count;
+        using (MySqlCommand commandCount = new MySqlCommand(cmdCount, connection))
+        {
+            commandCount.Parameters.AddWithValue("@table", table);
+            count = Convert.ToInt32(commandCount.ExecuteScalar());
+        }
+        
+        string cmdText = $"SELECT * FROM {table}";
+        MySqlCommand command = new MySqlCommand(cmdText, connection);
+        command.Parameters.AddWithValue("@table", table);
+        using (MySqlDataReader reader = command.ExecuteReader())
+        {
+            string[][] message = new string[count][];
+            int i = 0;
+            while (reader.Read())
+            {
+                message[i] = new string[3];
+                message[i][0] = reader["id"].ToString();
+                message[i][1] = reader["task"].ToString();
+                message[i][2] = reader["description"].ToString();
+                i++;
+            }
+            return message;
+        }
+    }
 
     public static void SQLInsert(MySqlConnection connection, string table, string newTask, string newDescription)
     {
@@ -33,12 +62,77 @@ public static class SQLLogic
 
     public static void SQLDelete(MySqlConnection connection, string table, string oldTask, string oldDescription)
     {
-        string cmdText = $"DELETE FROM {table} WHERE task = @task AND description = @description";
+        string cmdText = $"""
+                           DELETE FROM {table} WHERE task = @task AND description = @description;
+                           """;
         using (MySqlCommand command = new MySqlCommand(cmdText, connection))
         {
             command.Parameters.AddWithValue("@task", oldTask);
             command.Parameters.AddWithValue("@description", oldDescription);
             command.ExecuteNonQuery();
         }
+        
+        string cmdMax = $"""
+                         SELECT IFNULL(MAX(id), 0) FROM {table};
+                         """;
+        int idMax;
+        using (MySqlCommand commandMax = new MySqlCommand(cmdMax, connection))
+        {
+            idMax = Convert.ToInt32(commandMax.ExecuteScalar());
+        }
+        string cmdInc = $"""
+                         ALTER TABLE {table} AUTO_INCREMENT = @max;
+                         """;
+        using (MySqlCommand commandInc = new MySqlCommand(cmdInc, connection))
+        {
+            commandInc.Parameters.AddWithValue("@max", idMax + 1);
+            commandInc.ExecuteNonQuery();
+        }
+    }
+
+    public static string PanelToTable(int panel)
+    {
+        string table;
+        switch (panel)
+        {
+            case 0:
+                table = "tasks_needed";
+                break;
+            case 1:
+                table = "tasks_top_priority";
+                break;
+            case 2:
+                table = "tasks_in_progress";
+                break;
+            case 3:
+                table = "tasks_finished";
+                break;
+            default:
+                throw new ArgumentException("Invalid panel");
+        }
+        return table;
+    }
+    
+    public static string PanelToTable(string panel)
+    {
+        string table;
+        switch (Convert.ToInt32(panel))
+        {
+            case 0:
+                table = "tasks_needed";
+                break;
+            case 1:
+                table = "tasks_top_priority";
+                break;
+            case 2:
+                table = "tasks_in_progress";
+                break;
+            case 3:
+                table = "tasks_finished";
+                break;
+            default:
+                throw new ArgumentException("Invalid panel");
+        }
+        return table;
     }
 }
--- TODOshka/MainWindow.xaml.cs
+++ TODOshka/MainWindow.xaml.cs
@@ -1,69 +1,201 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Net.Mime;
+using System.Runtime.CompilerServices;
+using System.Security.RightsManagement;
+using System.Windows;
+using System.Windows.Automation;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using MySql.Data.MySqlClient;
 using static SQLLogic.SQLLogic;
 
 namespace TODOshka;
-
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
 public partial class MainWindow : Window
 {
+    static string connectionString = "server=localhost;port=3306;database=TODOshka;user=user1;password=sussybaka;";
     public MainWindow()
     {
         InitializeComponent();
+        AddDataToStackPanel(0);
+        AddDataToStackPanel(1);
+        AddDataToStackPanel(2);
+        AddDataToStackPanel(3);
     }
 
-    public void ButtonClickRead(object sender, RoutedEventArgs e)
+    private void AddDataToStackPanel(int panel)
     {
-        string connectionString = "server=localhost;port=3306;database=TODOshka;user=user1;password=sussybaka;";
-        using (MySqlConnection connection = new MySqlConnection(connectionString))
+        try
         {
-            try
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
             {
                 connection.Open();
-                MessageBox.Show(SQLReadAll(connection, "tasks_needed"));
+                string table = PanelToTable(panel);
+                string[][] newData = SQLReadAllMatrix(connection, table);
+                int count = newData.Length;
+                
+                for (int i = 0; i < count; i++)
+                {
+                    CreateCuteBox(newData[i], panel);
+                }
+                connection.Close();
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+        }
+        catch (Exception e)
+        {
+            MessageBox.Show(e.Message);
         }
     }
 
-    public void ButtonClickAdd(object sender, RoutedEventArgs e)
+    private void CreateCuteBox(string[] data, int panel)
     {
-        string connectionString = "server=localhost;port=3306;database=TODOshka;user=user1;password=sussybaka;";
+        Border box = new Border
+        {
+            Height = 100,
+            BorderBrush = Brushes.Black,
+            BorderThickness = new Thickness(5),
+            CornerRadius = new CornerRadius(5),
+            Margin = new Thickness(10),
+            Background = Brushes.Aquamarine
+        };
+        Grid.SetRow(box, 1);
+        Grid.SetColumn(box, panel);
+
+        StackPanel mainStack = new StackPanel
+        {
+            Orientation = Orientation.Vertical
+        };
+
+        Grid taskStack = new Grid
+        {
+            Background = Brushes.Blue,   
+        };
+        taskStack.ColumnDefinitions.Add(new ColumnDefinition
+        {
+            Width = new GridLength(1, GridUnitType.Star)
+        });
+        taskStack.ColumnDefinitions.Add(new ColumnDefinition{Width = GridLength.Auto});
         
-        using (MySqlConnection connection = new MySqlConnection(connectionString))
+        TextBlock task = new TextBlock
         {
-            try
+            Text = data[1],
+            FontSize = 18,
+            FontWeight = FontWeights.Bold
+        };
+
+        Button deleteButton = new Button
+        {
+            Height = 20,
+            Width = 20,
+            HorizontalAlignment = HorizontalAlignment.Right,
+            Content = new Image
             {
-                connection.Open();
-                SQLInsert(connection, "tasks_needed", TaskAdd.Text, DescriptionAdd.Text);
+                Source = new BitmapImage(new Uri("pack://application:,,,/TODOshka;component/Resources/bin.png")),
+                Stretch = Stretch.Uniform
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+        };
+        deleteButton.Click += DeleteButton_Click;
+        string[] tagData = new string[4];
+        data.CopyTo(tagData, 0);
+        tagData[3] = panel.ToString();
+        deleteButton.Tag = tagData;
+        
+        
+        TextBlock description = new TextBlock
+        {
+            Text = data[2]
+        };
+
+        Grid descriptionGrid = new Grid
+        {
+            VerticalAlignment = VerticalAlignment.Stretch
+        };
+        
+        taskStack.Children.Add(task);
+        taskStack.Children.Add(deleteButton);
+        mainStack.Children.Add(taskStack);
+        
+        descriptionGrid.Children.Add(description);
+        mainStack.Children.Add(descriptionGrid);
+        
+        box.Child = mainStack;
+
+        switch (panel)
+        {
+            case 0:
+                PanelTasksNeeded0.Children.Add(box);
+                break;
+            case 1:
+                PanelTasksNeeded1.Children.Add(box);
+                break;
+            case 2:
+                PanelTasksNeeded2.Children.Add(box);
+                break;
+            case 3:
+                PanelTasksNeeded3.Children.Add(box);
+                break;
+            default:
+                throw new ArgumentException("Invalid panel in create box");
         }
     }
 
-    public void ButtonClickDelete(object sender, RoutedEventArgs e)
+    private void DeleteButton_Click(object sender, RoutedEventArgs e)
     {
-        string connectionString = "server=localhost;port=3306;database=TODOshka;user=user1;password=sussybaka;";
-
-        using (MySqlConnection connection = new MySqlConnection(connectionString))
+        Button button = sender as Button;
+        string[] data = button.Tag as string[];
+        try
         {
-            try
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
             {
                 connection.Open();
-                SQLDelete(connection, "tasks_needed", TaskDelete.Text, DescriptionDelete.Text);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
+                SQLDelete(connection, PanelToTable(data[3]), data[1], data[2]);
+                RefreshPanel(int.Parse(data[3]));
+                connection.Close();
             }
         }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+    }
+
+    private void AddButton_Click(object sender, RoutedEventArgs e)
+    {
+        Button button = sender as Button;
+        string data = button.Tag as string;
+        Random random = new Random();
+        using (MySqlConnection connection = new MySqlConnection(connectionString))
+        {
+            connection.Open();
+            SQLInsert(connection, PanelToTable(data), random.Next().ToString(), random.Next().ToString());
+            RefreshPanel(int.Parse(data));
+            connection.Close();
+        }
+    }
+
+
+    private void RefreshPanel(int panel)
+    {
+        switch (panel)
+        {
+            case 0:
+                PanelTasksNeeded0.Children.Clear();
+                break;
+            case 1:
+                PanelTasksNeeded1.Children.Clear();
+                break;
+            case 2:
+                PanelTasksNeeded2.Children.Clear();
+                break;
+            case 3:
+                PanelTasksNeeded3.Children.Clear();
+                break;
+            default:
+                throw new ArgumentException("Invalid panel in refresh panel");
+        }
+        AddDataToStackPanel(panel);
     }
 }
